d5775856e98ffe6accc087d340b0807406c52322
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-23 00:41:35 +0700

    Add files via upload

---
+++ PROGRAM  UNTUK MEMERIKSA BILANGAN GANJIL ATAU GENAP.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x;
+	printf("masukan bilangan ; ");
+	scanf("%d",&x);
+	if(x%2==0)
+	{
+		printf("%d adalah bilangan genap\n\n",x);
+		
+	}
+	else
+	{
+		printf("%d adalah bilangan ganjil\n\n,x");
+		
+	}
+	printf("PAUSE");
+}
